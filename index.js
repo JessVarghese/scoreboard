ad2f7d3c2765446b3d00cbfb0498973c1fd2af16
Jess Varghese <jessvarghese@gmail.com>
2023-01-22 20:58:13 -0500

    Added function to add two and three points

--- index.js
+++ index.js
@@ -6,14 +6,39 @@ let guestCount = document.getElementById("score2-text")
 
 
 
-function addHome() {
+function addHome1() {
     scoreHome +=1
     homeCount.textContent = scoreHome
 
 }
 
+function addHome2() {
+    scoreHome +=2
+    homeCount.textContent = scoreHome
 
-function addGuest() {
+}
+
+function addHome3() {
+    scoreHome +=3
+    homeCount.textContent = scoreHome
+
+}
+
+function addGuest1() {
+    scoreGuest += 1
+    guestCount.textContent = scoreGuest
+}
+
+function addGuest2() {
+    scoreGuest += 2
+    guestCount.textContent = scoreGuest
+}
+
+function addGuest3() {
+    scoreGuest += 3
+    guestCount.textContent = scoreGuest
+}
+function addGuest1() {
     scoreGuest += 1
     guestCount.textContent = scoreGuest
 }
