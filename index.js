d912ee60a5b81bd79d369d8104138929884af7c2
Jess Varghese <jessvarghese@gmail.com>
2023-01-22 19:59:09 -0500

    intial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let scoreHome = 0
+let scoreGuest = 0
+
+let homeCount = document.getElementById("score1-text")
+let guestCount = document.getElementById("score2-text")
+
+
+function addHome() {
+    scoreHome +=1
+    homeCount.textContent = scoreHome
+
+}
+
+
+function addGuest() {
+    scoreGuest += 1
+    guestCount.textContent = scoreGuest
+}
